ba3bc0b109b3026f4152769b455023527eacae7b
JayChae <86304360+JayChae@users.noreply.github.com>
2022-11-15 10:03:44 +0900

    map first
    map first

---
+++ map/app/src/main/java/com/example/map/MapsActivity.kt
@@ -0,0 +1,50 @@
+package com.example.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.map.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val locations = mapOf<String,LatLng>("글로벌프라자" to LatLng(35.8896997627053,128.61168939232567),"북문" to LatLng(35.889070663727615,128.60887490090266))
+
+        for ((key,value) in locations) {
+            mMap.addMarker(MarkerOptions().position(value).title("${key}"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(value))
+
+        }
+    }
+}
