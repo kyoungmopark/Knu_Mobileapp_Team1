36f215a59db6a440e0470e36d151fddd555123c7
kdozlo <eodud5730@naver.com>
2022-12-17 05:00:23 +0900

    Saving junggu data in firebase

--- map/server/src/main/java/com/example/server/MainActivity.kt
+++ map/server/src/main/java/com/example/server/MainActivity.kt
@@ -87,21 +87,47 @@ class MainActivity : AppCompatActivity() {
         val service = RawDataService(DaeguJungguData::class, "https://api.odcloud.kr/api/15101506/v1/uddi:1958c212-87f4-4042-9466-eb03cb718ff1?perPage=0&serviceKey=N5y%2B8eZU60ZCyJtY9JpQwgjexI5cM5LAK8S4s1p3WHgtTMXy24R4z%2Bt7njRjWjXdjVwteW39U5SPpLsLcAnB%2Fg%3D%3D")
         val rawDataList = service.receiveByGroup()
         val mapDataList = mutableListOf<MapData>()
+        var db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-        for (rawData in rawDataList) {
-            try {
-                Log.d("UPLOAD", "${rawData.key}")
-                val address = geocoder.getFromLocationName(rawData.key, 1)[0]
-                val geoPoint = GeoPoint(address.latitude, address.longitude)
-                val equipments = listOf<String>()
-                mapDataList.add(MapData(geoPoint, equipments))
-            } catch (e: Exception) {
-                Log.d("UPLOAD", e.toString())
+
+        db.collection("junggu").document("0").get().addOnSuccessListener { document ->
+            class TempTotalCnt {
+                var total: Int = 0
+            }
+            val temptotalcnt = document.toObject(TempTotalCnt::class.java)
+            if ((document != null) && (temptotalcnt?.total == rawDataList.size)) {
+                Log.d("showdisplay", "Already update")
+            } else {
+                Log.d("showdisplay", "Updating")
+                for (rawData in rawDataList) {
+                    try {
+                        Log.d("UPLOAD", "${rawData.key}")
+                        val address = geocoder.getFromLocationName(rawData.key, 1)[0]
+                        val geoPoint = GeoPoint(address.latitude, address.longitude)
+                        val equipments = listOf<String>()
+                        mapDataList.add(MapData(geoPoint, equipments))
+                        Log.d("UPLOAD", "${rawData.key} -> $geoPoint")
+                    } catch (e: Exception) {
+                        val geoPoint = GeoPoint(0.0, 0.0)
+                        val equipments = listOf<String>(rawData.key, "Not to find latitude and longitude")
+                        mapDataList.add(MapData(geoPoint, equipments))
+                        Log.d("UPLOAD", e.toString())
+                    }
+                }
+
+                Log.d("rawDatatList size", "size: ${rawDataList.size}")
+
+                var cnt: Int = 0
+                for(mapData in mapDataList){
+                    cnt++
+                    db.collection("bukgu").document(cnt.toString()).set(mapData)
+                }
+
+                class Totalcnt(var total: Int)
+                var totalcnt = Totalcnt(cnt)
+                db.collection("bukgu").document("0").set(totalcnt)
+                Log.d("showdisplay", "Finish updating")
             }
         }
-
-
-
-
     }
 }
