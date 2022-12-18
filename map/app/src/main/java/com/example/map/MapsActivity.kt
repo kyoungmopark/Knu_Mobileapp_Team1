9b34552ef15aa1847d8f495d681ad66bb9921df5
kdozlo <eodud5730@naver.com>
2022-12-18 15:32:18 +0900

    show current location

--- map/app/src/main/java/com/example/map/MapsActivity.kt
+++ map/app/src/main/java/com/example/map/MapsActivity.kt
@@ -1,21 +1,36 @@
 package com.example.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.map.databinding.ActivityMapsBinding
 import com.example.mapdata.MapData
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.firestore.FirebaseFirestore
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
 
+    lateinit var providerClient: FusedLocationProviderClient
+    lateinit var apiClient: GoogleApiClient
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     lateinit var db: FirebaseFirestore
@@ -26,10 +41,119 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            if (it.all { permission -> permission.value == true }) {
+                // 위치 제공자 준비하기
+                apiClient.connect()
+            } else {
+                Toast.makeText(this, "권한 거부", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        providerClient = LocationServices.getFusedLocationProviderClient(this)
+        apiClient = GoogleApiClient.Builder(this)
+            .addApi(LocationServices.API)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .build()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !==
+            PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) !==
+            PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_NETWORK_STATE
+            ) !==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_NETWORK_STATE
+                )
+            )
+        } else {
+            // 위치 제공자 준비하기
+            apiClient.connect()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100 &&
+            grantResults[0] === PackageManager.PERMISSION_GRANTED &&
+            grantResults[1] === PackageManager.PERMISSION_GRANTED &&
+            grantResults[2] === PackageManager.PERMISSION_GRANTED
+        ) {
+            apiClient.connect()
+        }
+    }
+    private fun moveMap(latitude: Double, longitude: Double) {
+        val latLng = LatLng(latitude, longitude)
+        val position: CameraPosition = CameraPosition.Builder()
+            .target(latLng)
+            .zoom(16f)
+            .build()
+// 지도 중심 이동하기
+        mMap!!.moveCamera(CameraUpdateFactory.newCameraPosition(position))
+// 마커 옵션
+        val markerOptions = MarkerOptions()
+        markerOptions.icon(
+            BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_AZURE
+            )
+        )
+        markerOptions.position(latLng)
+        markerOptions.title("MyLocation")
+        // 마커 표시하기
+        mMap?.addMarker(markerOptions)
+    }
+
+    // 위치 제공자를 사용할 수 있는 상황일 때
+    override fun onConnected(p0: Bundle?) {
+        if (ContextCompat.checkSelfPermission(
+                this@MapsActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) === PackageManager.PERMISSION_GRANTED
+        ) {
+            providerClient.getLastLocation().addOnSuccessListener(
+                this@MapsActivity,
+                object : OnSuccessListener<Location> {
+                    override fun onSuccess(location: Location?) {
+                        location?.let {
+                            val latitude = location.latitude
+                            val longitude = location.longitude
+                            // 지도 중심 이동하기
+                            moveMap(latitude, longitude)
+                        }
+                    }
+                })
+            apiClient.disconnect()
+        }
+    }
+    override fun onConnectionSuspended(p0: Int) {
+        // 위치 제공자를 사용할 수 없을 때
+    }
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        // 사용할 수 있는 위치 제공자가 없을 때
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -37,10 +161,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val firebaseMapDataList = mutableListOf<MapData>()
         db = FirebaseFirestore.getInstance()
 
-        val position = CameraPosition.Builder()
+       /* val position = CameraPosition.Builder()
             .target(LatLng(35.893190, 128.610165)).zoom(16f).build()
 
-        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(position))
+        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(position))*/
 
         db.collection("bukgu")
             .get().addOnSuccessListener { document ->
